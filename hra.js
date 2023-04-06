5a84fc1d0fa2b8e79ef1965ff2348a1d027754cb
Nataliia Hladii <natalia14101995@gmail.com>
2023-04-06 15:14:08 +0200

    Vytvoření funkce pro přidání kruhu a kříže

--- hra.js
+++ hra.js
@@ -1,1 +1,31 @@
-console.log('funguju')
+let currentPlayer = 'circle';
+
+const elements = document.querySelectorAll('.game_button');
+
+const changeSing = (event) => {
+    if (currentPlayer === 'circle') {
+        event.target.querySelector('span').classList.add('game_field--circle')
+        document.querySelector('.player span').classList.add('cross')
+        document.querySelector('.player span').classList.remove('circle')
+    } else if (currentPlayer === 'cross') {
+        event.target.querySelector('span').classList.add('game_field--cross')
+        document.querySelector('.player span').classList.remove('cross')
+        document.querySelector('.player span').classList.add('circle')
+    }
+    event.target.disabled = true;
+    changePlayer()
+};
+const changePlayer = () => {
+   console.log(currentPlayer)
+    if (currentPlayer === 'circle') {
+        currentPlayer = 'cross'
+    } else if (currentPlayer === 'cross') {
+        currentPlayer = 'circle'
+    }
+}
+
+elements.forEach((item) => {
+    item.addEventListener('click', changeSing)
+})
+
+
